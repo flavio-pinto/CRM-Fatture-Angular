39e67a31b5520befa1871dd37bdf5e8910186995
Flavio Pinto <flaviopnt@hotmail.it>
2022-03-10 08:20:21 +0100

    Navbar creata

---
+++ src/app/components/navbar.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  template: `
+    <nav class="navbar navbar-expand navbar-light bg-light">
+    <div class="container-fluid">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarTogglerDemo01"
+        aria-controls="navbarTogglerDemo01"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a
+            class="nav-link active"
+            aria-current="page"
+              >Home</a
+            >
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              >Login</a
+            >
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              >Signup</a
+            >
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              >Utenti</a
+            >
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              >Clienti</a
+            >
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              >Fatture</a
+            >
+          </li>
+        </ul>
+        <!-- <div *ngIf="isLoggedIn">
+          <p class="d-inline username-welc-back">Bentornato <span class="fst-italic fw-bolder">{{welcomeUser}}</span></p>
+          <button class="btn btn-danger mx-3" (click)="onLogout()">logout</button>
+        </div> -->
+      </div>
+    </div>
+  </nav>
+
+  `,
+  styles: [
+  ]
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
