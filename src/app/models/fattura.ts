89fec87538c5463d6546914c99f05fcaecd379d2
Flavio Pinto <flaviopnt@hotmail.it>
2022-03-11 12:20:58 +0100

    Corretto model fattura

--- src/app/models/fattura.ts
+++ src/app/models/fattura.ts
@@ -1,14 +1,59 @@
 export interface Fattura {
-  id : number;
-  username: string;
-  email: string;
-  password: string;
-  nome: string;
-  cognome: string;
-  roles: [
-    {
+  id: number;
+  data: string;
+  numero: number;
+  anno: number;
+  importo: number;
+  stato: {
+    id: number,
+    nome: string
+  };
+  cliente: {
+    id: number;
+    ragioneSociale: string;
+    partitaIva: string;
+    tipoCliente: string;
+    email: string;
+    pec: string;
+    telefono: string;
+    nomeContatto: string;
+    cognomeContatto: string;
+    telefonoContatto: string;
+    emailContatto: string;
+    indirizzoSedeOperativa: {
       id: number,
-      roleName: string
-    }
-  ]
+      via: string,
+      civico: string,
+      cap: string,
+      localita: string,
+      comune: {
+        id: number,
+        nome: string,
+        provincia: {
+          id: number,
+          nome: string,
+          sigla: string
+        }
+      }
+    };
+    indirizzoSedeLegale: {
+      id: number,
+      via: string,
+      civico: string,
+      cap: string,
+      localita: string,
+      comune: {
+        id: number,
+        nome: string,
+        provincia: {
+          id: number,
+          nome: string,
+          sigla: string
+        }
+      }
+    };
+    dataInserimento: string;
+    dataUltimoContatto: string;
+    fatturatoAnnuale: number;
+  }
 }
